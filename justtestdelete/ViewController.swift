73c7edd38fc8f67874c9d85c79bdce79d4214f0b
Daniel Esposito <danielesposito@macbkk.com>
2016-03-11 11:09:06 +0700

    Added first connections of elements and NSTimer/NSDate

--- justtestdelete/ViewController.swift
+++ justtestdelete/ViewController.swift
@@ -8,15 +8,36 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    var myTimer = NSTimer()
+    var currentDate = NSDate()
+    var startDate = NSDate()
+    var stopDate = NSDate()
+    
+    
+    @IBOutlet weak var setCounterLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButtonLabel: UIButton!
+    @IBOutlet weak var resetButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-/// Deleted Memory Function
+    
+    
+    @IBAction func startButtonPressed(sender: AnyObject)
+    {
+        
+    }
 
-
+    @IBAction func resetButtonPressed(sender: AnyObject)
+    {
+        
+    }
 }
 
